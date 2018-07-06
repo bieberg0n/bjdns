766baaa0531f828a6f9233959a20ef361038ba32
bjong <biebergong@gmail.com>
2018-07-06 11:25:44 +0800

    完善readme

--- config_example.py
+++ config_example.py
@@ -3,8 +3,8 @@ import multiprocessing
 
 bind = '0.0.0.0:5353'
 workers = multiprocessing.cpu_count() * 2 + 1
-keepalive = 120
 worker_class = 'gevent'
+keepalive = 120
 # keyfile = ''
 # certfile = ''
 # ssl_version = 5
